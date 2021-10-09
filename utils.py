c5c6a7967c4d70b5cf603f3bf390dc3a810f9efa
elwinjyot <elwinjyothisr444@gmail.com>
2021-10-09 22:28:37 +0530

    Updated Utils with documentation and methods

--- utils.py
+++ utils.py
@@ -1,3 +1,38 @@
+import os
+
+'''
+utils.py script contains a class `Tools`
+consisting of all essential functions required
+for the application to run.
+
+ -  METHOD LIST
+[1] Initialization
+[2] Add family member
+[3] Remove family member
+[4] Edit family member
+[5] Plot monthly report'''
 
 class Tools:
-    test = None
+    # Initialization
+    def __init__(self):
+        self.BASE = os.getcwd()
+        self.filePath = os.path.join(self.BASE, 'database.csv')
+        
+        if 'database.csv' not in os.listdir():
+            open(os.path.join(self.BASE, "database.csv"), "w").close()
+
+    # Add family members to database
+    def addMember(self):
+        pass
+
+    # Remove family members from database
+    def removeMember(self):
+        pass
+
+    # Edit family member details
+    def updateMember(self):
+        pass
+
+    # Plot monthly report
+    def plotGraph(self):
+        pass
